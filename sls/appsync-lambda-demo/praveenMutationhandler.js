0ebaa891bc767211f1741bcd33b94c5f959d8d93
amardeep05011990 <amardeep05011990@gmail.com>
2025-08-20 17:34:10 +0530

    added todo in serverless appsync

--- sls/appsync-lambda-demo/praveenMutationhandler.js
+++ sls/appsync-lambda-demo/praveenMutationhandler.js
@@ -1,10 +1,38 @@
+import AWS from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";   // to generate unique IDs
 
-// export const abc = async (event) => {
-//   console.log("Event:", JSON.stringify(event, null, 2));
-//   const name = event.arguments?.role || "Software Developer" ; // AppSync passes args here
-//   return `Hello ${name}`;
-// };
+const dynamodb = new AWS.DynamoDB();
 
 export const abc = async (event) => {
-    
- };
+  console.log("paraveen mutation data :", JSON.stringify(event, null, 2));
+
+  // Get GraphQL mutation arguments
+  const { title, author } = event.arguments;
+
+  // Generate a unique ID for the item
+  const id = uuidv4();
+
+  const params = {
+    Item: {
+      "id": { S: id },
+      "title": { S: title },
+      "author": { S: author }
+    },
+    TableName: process.env.TODO_TABLE_NAME,
+    ReturnConsumedCapacity: "TOTAL"
+  };
+
+  try {
+    await dynamodb.putItem(params).promise();
+
+    // Return back the stored item
+    return {
+      id,
+      title,
+      author
+    };
+  } catch (err) {
+    console.error("DynamoDB Error:", err);
+    throw new Error("Failed to save item");
+  }
+};
