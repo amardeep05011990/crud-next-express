b0cd03a16e96bf6bab56f7067c28f8e4ca289fd7
amardeep05011990 <amardeep05011990@gmail.com>
2025-08-26 23:50:43 +0530

    Added sls/appsync-lambda-demo as a clean submodule

--- sls/appsync-lambda-demo/amardeephandler.js
+++
@@ -1,6 +0,0 @@
-
-export const xyz = async (event) => {
-  console.log("Event:", JSON.stringify(event, null, 2));
-  const name = event.arguments?.name || "World" ; // AppSync passes args here
-  return `Hello ${name}`;
-};
--- sls/appsync-lambda-demo/praveenhandler.js
+++
@@ -1,6 +0,0 @@
-
-export const klm = async (event) => {
-  console.log("Event:", JSON.stringify(event, null, 2));
-  const name = event.arguments?.address || "India" ; // AppSync passes args here
-  return `Hello ${name}`;
-};
